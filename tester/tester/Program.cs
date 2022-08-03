7fadf8f8043e749025c4899bfb3954a9bac7e70c
Asgar Y. Asgarov <asgaryas@code.local>
2022-08-03 18:52:19 +0400

    checker

---
+++ tester/tester/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "agilli cirmaninca ,deli vurdu cayi kecdi";
+            bool cas= false;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i]=='a')
+                {
+                    cas = true; 
+                }
+            }
+            if (cas)
+            {
+                Console.WriteLine("there is a charachter");
+            }
+            else
+            {
+                Console.WriteLine("there is no a charachter");
+            }
+        }
+
+    }
+}
